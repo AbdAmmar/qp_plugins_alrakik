cd1f7dafd1c1a87efa871c6b68b46f59fd7646c3
Abdallah Ammar <abd.ammar.phys@gmail.com>
2024-08-24 20:38:58 +0200

    added torus overlap

--- torus/torus.irp.f
+++ torus/torus.irp.f
@@ -12,21 +12,20 @@ subroutine main()
 
   implicit none
   integer                       :: i, j
+  double precision              :: test_sym
   double precision, allocatable :: eigvalues(:), eigvectors(:,:)
 
   PROVIDE ao_overlap
 
-!  print*, " ao_overlap : "
-!  do i = 1, ao_num
-!    do j = 1, ao_num
-!      print *, j, i, ao_overlap(j,i)
-!    enddo
-!  enddo
+  print*, " ao_overlap : "
+  do j = 1, ao_num
+    write(*,"(1000(f15.7,2x))") (ao_overlap(j,i), i = 1, ao_num)
+  enddo
 
 
   allocate(eigvalues(ao_num), eigvectors(ao_num,ao_num))
 
-  call lapack_diag(eigvalues, eigvectors, ao_overlap, ao_num, ao_num)
+  call lapack_diag(eigvalues(1), eigvectors(1,1), ao_overlap(1,1), ao_num, ao_num)
 
   print*, "eigvalues:"
   do i = 1, ao_num
